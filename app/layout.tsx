ea403d3e857b4e83e1137b10b95794129c3c8a86
Fadhlullah Mustapha <fadhmusty@gmail.com>
2025-12-08 10:26:22 +0100

    revamped

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "SCOTT Headphones",
-  description: "Premium headphones ",
+  description: "Premium headphones by SCOTT",
 };
 
 export default function RootLayout({
